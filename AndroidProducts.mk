c497f0fa7688c2103e0411ae682bc8e1948c6392
TRISF <trisfaizal@gmail.com>
2021-02-08 12:04:27 +0700

    sagit: aosp

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/komodo_sagit.mk
+    $(LOCAL_DIR)/aosp_sagit.mk
--- aosp_sagit.mk
+++ aosp_sagit.mk
@@ -21,27 +21,27 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sagit device
 $(call inherit-product, device/xiaomi/sagit/device.mk)
 
-# Inherit some common Reptile stuff.
-$(call inherit-product, vendor/komodo/config/common_full_phone.mk)
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # MiuiCamera
 $(call inherit-product-if-exists, vendor/apps/MiuiCamera/config.mk)
 
-PRODUCT_NAME := komodo_sagit
+PRODUCT_NAME := aosp_sagit
 PRODUCT_DEVICE := sagit
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 6
 PRODUCT_MANUFACTURER := Xiaomi
 
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    org.komodo.maintainer=trisf
-
-KOMODO_VARIANT := RELEASE
-CURRENT_BUILD_TYPE := nogapps
-
+TARGET_FACE_UNLOCK := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_BOOT_ANIMATION_RES := 1080
 USE_PIXEL_CHARGING := true
-TARGET_FACE_UNLOCK := true
+
+KBUILD_BUILD_USER := trisf
+KBUILD_BUILD_HOST := xfi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
